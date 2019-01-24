e4d0de745071aa11567310c6fe82bf979bcb6b71
Petko D. Petkov <pdp@gnucitizen.org>
2019-01-24 05:51:40 +0000

    Added system scheduler.

--- test/scheduler.js
+++ test/scheduler.js
@@ -1,10 +1,36 @@
-const { Scheduler } = require('../lib/scheduler')
+const { Scheduler, SystemScheduler } = require('../lib/scheduler')
 
 const assert = require('assert')
 
 describe('scheduler', () => {
     describe('#fetch()', () => {
         const scheduler = new Scheduler()
+
+        it('must fetch http request', async() => {
+            const tran = await scheduler.fetch('http://httpbin.org/get')
+
+            const { responseBody } = tran
+
+            const { url } = JSON.parse(responseBody.toString())
+
+            assert.ok(url === 'http://httpbin.org/get', 'url === http://httpbin.org/get')
+        })
+
+        it('must fetch https request', async() => {
+            const tran = await scheduler.fetch('https://httpbin.org/get')
+
+            const { responseBody } = tran
+
+            const { url } = JSON.parse(responseBody.toString())
+
+            assert.ok(url === 'https://httpbin.org/get', 'url === https://httpbin.org/get')
+        })
+    })
+})
+
+describe('systemScheduler', () => {
+    describe('#fetch()', () => {
+        const scheduler = new SystemScheduler()
 
         it('must fetch http request', async() => {
             const tran = await scheduler.fetch('http://httpbin.org/get')
